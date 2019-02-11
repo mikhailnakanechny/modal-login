52a896ad7e51f6283caf5fa93103e8831a1197c6
mikhailnakanechny <mikhailnakanechny@gmail.com>
2019-02-12 00:59:23 +0300

    feat: add validation for first form

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,29 @@ const passHideRegister = document.getElementById('signup-pass');
 const btnPassHideLogin = document.querySelector('.hide-pass-login');
 const btnPassHideRegister = document.querySelector('.hide-pass-register');
 
+let indexStart = 0;
+let indexEnd = 3;
+let indexErr = 0;
+
+function indexSet() {
+  if (!loginForm.hidden) {
+    indexStart = 0;
+    indexEnd = 3;
+    indexErr = 0;
+  } else if (!registerForm.hidden) {
+    indexStart = 4;
+    indexEnd = 9;
+    indexErr = 1;
+  } else if (!resetForm.hidden) {
+    indexStart = 10;
+    indexEnd = 11;
+    indexErr = 2;
+  }
+
+  console.log(indexStart);
+  console.log(indexEnd);
+}
+
 function loginSelected() {
   event.preventDefault();
   authModal.hidden = false;
@@ -18,6 +41,7 @@ function loginSelected() {
   resetForm.hidden = true;
   tabLogin.classList.add('selected');
   tabRegister.classList.remove('selected');
+  indexSet();
 }
 
 function registerSelected() {
@@ -28,6 +52,7 @@ function registerSelected() {
   resetForm.hidden = true;
   tabRegister.classList.add('selected');
   tabLogin.classList.remove('selected');
+  indexSet();
 }
 
 function resetSelected() {
@@ -36,6 +61,7 @@ function resetSelected() {
   loginForm.hidden = true;
   registerForm.hidden = true;
   resetForm.hidden = false;
+  indexSet();
 }
 
 function hideModal() {
@@ -44,6 +70,7 @@ function hideModal() {
   loginForm.hidden = true;
   registerForm.hidden = true;
   resetForm.hidden = true;
+  indexSet();
 }
 
 function hidePassword(passInput) {
@@ -92,25 +119,69 @@ document.onkeydown = function keyPress(e) {
 
 const elements = document.getElementsByTagName('INPUT');
 const arrErr = document.querySelectorAll('.c-auth__error');
+const patternEmail = /[a-zA-Z0-9]{3,}@[a-zA-Z]{3,}[.]{1}[a-zA-Z]{2,}/;
+const patternPassword = /((?=.*\d)(?=.*[a-zA-Z]).{3,})/;
+const patternUser = /[a-zA-Z]+[a-zA-Z0-9]{2,}/;
 
-for (let i = 0; i < elements.length; i += 1) {
-  if (elements[i].type.toLowerCase() === 'submit') {
-    elements[i].onclick = () => {
-      arrErr.forEach((elm) => {
-        elm.hidden = true;
-      });
-    };
+function validate(element, errText, errField, regExp) {
+  const resultRegTest = regExp.test(element.value);
+  if (!element.value || resultRegTest) {
+    element.style.border = '2px solid red';
+    errField.innerHtml = errText;
+    errField.hidden = false;
   }
-
-  elements[i].oninvalid = () => {
-    if ((i >= 0) && (i <= 3)) {
-      arrErr[0].hidden = false;
-    }
-    if ((i >= 4) && (i <= 8)) {
-      arrErr[1].hidden = false;
-    }
-    if ((i >= 9) && (i <= 10)) {
-      arrErr[2].hidden = false;
-    }
-  };
 }
+
+console.log(elements);
+
+(function validationView() {
+  for (let i = indexStart; i <= indexEnd; i += 1) {
+    if (elements[i].type.toLowerCase() === 'submit') {
+      elements[i].onclick = (e) => {
+        e.preventDefault();
+        console.log(indexErr);
+        arrErr.forEach((elm) => {
+          elm.hidden = true;
+        });
+
+        for (let j = indexStart; j <= indexEnd; j += 1) {
+          const errField = arrErr[indexErr];
+          console.log(indexEnd);
+          console.log(errField);
+          if (elements[j].type.toLowerCase() === 'email') {
+            validate(elements[j], 'check entered email', errField, patternEmail);
+          }
+        }
+      };
+    }
+  }
+}());
+
+// placeholder="E-mail"
+// pattern="[a-zA-Z0-9]{3,}@[a-zA-Z]{3,}[.]{1}[a-zA-Z]{2,}"
+// placeholder="Password"
+// pattern="((?=.*\d)(?=.*[a-zA-Z]).{3,})"
+// placeholder="Username"
+// pattern="[a-zA-Z]{1,}[a-zA-Z0-9]{2,}"
+//   for (let i = 0; i < elements.length; i += 1) {
+//     if (elements[i].type.toLowerCase() === 'submit') {
+//       elements[i].onclick = (e) => {
+//         e.preventDefault();
+//         arrErr.forEach((elm) => {
+//           elm.hidden = true;
+//         });
+//       };
+//     }
+//
+//     elements[i].oninvalid = () => {
+//       if ((i >= 0) && (i <= 3)) {
+//         arrErr[0].hidden = false;
+//       }
+//       if ((i >= 4) && (i <= 9)) {
+//         arrErr[1].hidden = false;
+//       }
+//       if ((i >= 10) && (i <= 11)) {
+//         arrErr[2].hidden = false;
+//       }
+//     };
+//   }
