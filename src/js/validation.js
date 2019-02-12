7cc5e460dd30ce331ad94815de4be017932d8da2
mikhailnakanechny <mikhailnakanechny@gmail.com>
2019-02-12 17:25:54 +0300

    feat: add passwords match checking

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,8 +1,11 @@
 const patternEmail = /[a-zA-Z0-9]{3,}@[a-zA-Z]{3,}[.]{1}[a-zA-Z]{2,}/;
 const patternPassword = /((?=.*\d)(?=.*[a-zA-Z]).{3,})/;
 const patternUser = /[a-zA-Z]+[a-zA-Z0-9]{2,}/;
+let passCount;
+let errCount;
+let err;
 
-function validate(element, errText, errField, regExp, err) {
+function validate(element, errText, errField, regExp) {
   const resultRegTest = regExp.test(element.value);
   if (!resultRegTest) {
     element.style.border = '2px solid red';
@@ -15,18 +18,29 @@ function validate(element, errText, errField, regExp, err) {
     errField.hidden = true;
     err = false;
   }
-  console.log(err);
 }
 
-function validateInputs(element, errField, err) {
+function validateInputs(element, errField) {
   if (element.type.toLowerCase() === 'text') {
-    validate(element, 'check entered username', errField, patternUser, err);
+    validate(element, 'check entered username', errField, patternUser);
   }
   if (element.type.toLowerCase() === 'email') {
-    validate(element, 'check entered email', errField, patternEmail, err);
+    validate(element, 'check entered email', errField, patternEmail);
   }
   if (element.type.toLowerCase() === 'password') {
-    validate(element, 'check entered password', errField, patternPassword, err);
+    validate(element, `check entered password${passCount}`, errField, patternPassword);
+    passCount += 1;
+  }
+  if (element.type.toLowerCase() === 'password') {
+    const password1 = document.getElementById('signup-pass1');
+    const password2 = document.getElementById('signup-pass2');
+    if ((password1.value !== password2.value) && (errCount === 1)) {
+      element.style.border = '2px solid red';
+      errField.innerHTML += '<p>passwords not match</p>';
+      errField.hidden = false;
+      err = false;
+      errCount += 1;
+    }
   }
 }
 
@@ -34,16 +48,14 @@ export function validateForm(form) {
   event.preventDefault();
   const errField = form.querySelector('.c-auth__error');
   errField.innerHTML = '';
-  let err = false;
+  err = false;
+  errCount = 1;
+  passCount = 1;
   const elmntArr = form.getElementsByTagName('input');
-  [].forEach.call(elmntArr, (element) => {
-    return validateInputs(element, errField, err);
-  });
-  if (err) {
-    return false;
-  }
-  return true;
+  [].forEach.call(elmntArr, element => validateInputs(element, errField, err, errCount, passCount));
+  return !err;
 }
+
 
 // (function validationView() {
 //   for (let i = indexStart; i <= indexEnd; i += 1) {
